9fbaeec4e4d06072dc3a91a3fc9d60b11301216b
Garth天卜 <g-art-h@outlook.com>
2024-07-23 18:56:41 +0800

    初步构建完成

--- src/main.rs
+++ src/main.rs
@@ -49,11 +49,7 @@ fn accumulate(n: usize, combinations: &DashMap<String, usize>, window: &mut Vec<
     }
 }
 
-fn count_combinations(
-    file_path: &str,
-    n: usize,
-    extra_chars: HashSet<char>,
-) -> io::Result<DashMap<String, usize>> {
+fn count_combinations(file_path: &str, n: usize, extra_chars: &HashSet<char>) -> io::Result<DashMap<String, usize>> {
     let file = File::open(file_path)?;
     let reader = BufReader::new(file);
 
@@ -87,21 +83,83 @@ fn count_combinations(
     Ok(combinations)
 }
 
-fn sort_combinations(combinations: DashMap<String, usize>) -> Vec<(String, usize)> {
-    let mut sorted_combinations: Vec<(String, usize)> = combinations.into_iter().collect();
-    sorted_combinations.sort_by(|a, b| b.1.cmp(&a.1));
-    sorted_combinations
+#[inline(always)]
+fn filter(n: usize, threshold: usize, combinations: &DashMap<String, usize>, words: &DashMap<String, usize>, window: &mut Vec<char>) {
+    let mut max_freq: usize = 0;
+    let mut max_word = String::with_capacity(n);
+
+    for i in 0..n {
+        let word: String = (&window[i..i + n]).iter().collect();
+        if let Some(freq) = combinations.get(&word) {
+            if *freq >= threshold && *freq > max_freq {
+                max_freq = *freq;
+                max_word = word;
+            }
+        }
+    }
+
+    let _ = window.split_off(n);
+    if max_freq > 0 {
+        *words.entry(max_word).or_insert(0) += 1;
+    }
 }
 
-fn write_results(
-    file_path: &str,
-    n: usize,
-    sorted_combinations: Vec<(String, usize)>,
-) -> io::Result<()> {
+fn count_words(file_path: &str, n: usize, extra_chars: &HashSet<char>, threshold: usize, combinations: DashMap<String, usize>) -> io::Result<DashMap<String, usize>> {
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    let lines: Vec<String> = reader.lines().collect::<Result<_, _>>()?;
+    let words: DashMap<String, usize> = DashMap::new();
+
+    let range = 2 * n - 1;
+
+    if extra_chars.is_empty() {
+        lines.into_par_iter().for_each(|line| {
+            let mut window: Vec<char> = Vec::with_capacity(range);
+            for c in line.chars() {
+                if c >= '一' && c <= '鿿' {
+                    window.push(c);
+                    if window.len() == range {
+                        filter(n, threshold, &combinations, &words, &mut window);
+                    }
+                } else {
+                    window.clear();
+                }
+            }
+        });
+    } else {
+        lines.into_par_iter().for_each(|line| {
+            let mut window: Vec<char> = Vec::with_capacity(range);
+            for c in line.chars() {
+                if c >= '一' && c <= '鿿' || extra_chars.contains(&c) {
+                    window.push(c);
+                    if window.len() == range {
+                        filter(n, threshold, &combinations, &words, &mut window);
+                    }
+                } else {
+                    window.clear();
+                }
+            }
+        });
+    }
+
+    Ok(combinations)
+}
+
+fn sort_words(words: DashMap<String, usize>) -> Vec<(String, usize)> {
+    let mut items: Vec<(String, usize)> = words
+        .iter()
+        .map(|entry| (entry.key().clone(), *entry.value()))
+        .collect();
+    items.sort_by(|a, b| b.1.cmp(&a.1));
+    items
+}
+
+fn write_results(file_path: &str, n: usize, sorted_words: Vec<(String, usize)>) -> io::Result<()> {
     let result_path = Path::new(file_path).with_file_name(format!("{n}字统计结果.txt"));
     let mut result_file = File::create(result_path)?;
 
-    let lines: Vec<_> = sorted_combinations
+    let lines: Vec<_> = sorted_words
         .into_iter()
         .map(|(combination, count)| format!("{combination}\t{count}\n"))
         .collect();
@@ -114,18 +172,16 @@ fn main() -> io::Result<()> {
     loop {
         let file_path = read_input::<String>("请输入文本文件路径：")?;
         let n = read_input::<usize>("请输入词长：")?;
-        let extra_chars = remove_chinese(&read_input::<String>(
-            "请输入要纳入的非汉字（输入为一行）：",
-        )?);
+        let extra_chars = remove_chinese(&read_input::<String>("请输入要纳入的非汉字（输入为一行）：")?);
         let threshold = get_threshold();
-        println!("使用阈值：{threshold}\n统计中...");
-        let combinations = count_combinations(&file_path, n, extra_chars)?;
-        println!("统计完成，筛选中...");
-        combinations.retain(|_, &mut value| value >= threshold);
-        println!("筛选完成，排序中...");
-        let sorted_combinations = sort_combinations(combinations);
-        println!("排序完成，输出中...");
-        write_results(&file_path, n, sorted_combinations)?;
-        println!("输出完成，再来一轮！\n");
+        println!("使用阈值：{threshold}\n逐字统计中...");
+        let combinations = count_combinations(&file_path, n, &extra_chars)?;
+        println!("逐字统计完成，进行盲分词统计...");
+        let words = count_words(&file_path, n, &extra_chars, threshold, combinations)?;
+        println!("盲分词统计完成，筛选中...");
+        words.retain(|_, &mut value| value >= threshold);
+        println!("筛选完成，输出中...");
+        write_results(&file_path, n, sort_words(words))?;
+        println!("输出完成，再来一次！\n");
     }
 }
